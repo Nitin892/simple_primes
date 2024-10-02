6e2a285890acb410c1d6845ef4f38f0bd3abf1c8
Nitin892 <nitinshenigaram7860@gmail.com>
2024-10-02 14:11:17 +0530

    second commit

--- simple_primes/urls.py
+++ simple_primes/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', include('primes.urls')),  # Include the primes app URLs
+    path('', include('primes.urls')),
 
 ]
